7d3454e6f4aa9b520e54e9e690da60b63177616d
BurakKoban <burakkoban@gmail.com>
2022-02-09 15:03:35 -0700

    New project

--- flask-05-Handling-SQL-with-Flask-Web-Application/app-with-mysql.py
+++ flask-05-Handling-SQL-with-Flask-Web-Application/app-with-mysql.py
@@ -110,3 +110,5 @@ def add_email():
 if __name__ == '__main__':
    # app.run(debug=True)
    app.run(host='0.0.0.0', port=80)
+
+   # sudo pip3 install flask flask-mysql sqlalchemy flask-SQLAlchemy
