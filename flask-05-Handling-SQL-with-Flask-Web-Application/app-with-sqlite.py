615bc14267aa9ec111e8c31fd83fc4977cfc7070
BurakKoban <burakkoban@gmail.com>
2022-02-09 15:55:32 -0700

    Sqlite_Project

--- flask-05-Handling-SQL-with-Flask-Web-Application/app-with-sqlite.py
+++ flask-05-Handling-SQL-with-Flask-Web-Application/app-with-sqlite.py
@@ -22,7 +22,7 @@ VALUES
 	("hilal", "hilal@micrasoft.com"),
 	("ayse", "ayse@facebook.com"),
     ("burak", "burak@tesla.com"),
-    ("etibar", "etibar@ebay.com")
+    ("etibar", "etibar@ebay.com"),
     ("engin","engin@google");
 """
 
@@ -97,5 +97,5 @@ def add_email():
 
 # - Add a statement to run the Flask application which can be reached from any host on port 80.
 if __name__=='__main__':
-   # app.run(debug=True)
-   app.run(host = "0.0.0.0", port=80)
+   app.run(debug=True)
+   # app.run(host = "0.0.0.0", port=80)
