712c93a08df68139f7ceebebbcd262607655f74e
BurakKoban <burakkoban@gmail.com>
2022-02-20 14:47:59 -0700

    Linux project

--- Project-101-Linux/Forth_Part/Task_2/multiline.sh
+++ Project-101-Linux/Forth_Part/Task_2/multiline.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 MY_KEY=$(cat certificate.pem)
-echo -e $MY_KEY >> new.pem
-# echo -e $(cat certificate.pem) >> new.pem
+echo -e $MY_KEY > new.pem
+# echo -e $(cat certificate.pem) > new.pem
--- Project-101-Linux/Forth_Part/Task_3/backup.sh
+++ Project-101-Linux/Forth_Part/Task_3/backup.sh
@@ -2,4 +2,5 @@
 
 grep "invalid user" auth.log | cut -d' ' -f8 | uniq -c > invalid_user.sh
 
-# grep "Invalid user" auth.log | cut -d' ' -f8 | uniq -c > invalid_user.sh
+# grep "invalid user" auth.log | cut -d' ' -f9 | uniq -c >> invalid_user.sh
+
