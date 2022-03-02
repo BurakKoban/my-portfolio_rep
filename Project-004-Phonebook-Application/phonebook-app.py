48c52f7d82d5ff0b88a42a565daf8a275df91a3e
BurakKoban <burakkoban@gmail.com>
2022-03-01 20:30:40 -0700

    Phonebook db app

--- Project-004-Phonebook-Application/phonebook-app.py
+++ Project-004-Phonebook-Application/phonebook-app.py
@@ -13,7 +13,7 @@ db_endpoint = open("/home/ec2-user/dbserver.endpoint", 'r', encoding='UTF-8')
 app.config['MYSQL_DATABASE_HOST'] = db_endpoint.readline().strip()
 app.config['MYSQL_DATABASE_USER'] = 'admin'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'Abcd1234'
-app.config['MYSQL_DATABASE_DB'] = 'clarusway_phonebook'
+app.config['MYSQL_DATABASE_DB'] = 'phonebook_db'
 app.config['MYSQL_DATABASE_PORT'] = 3306
 db_endpoint.close()
 mysql = MySQL()
